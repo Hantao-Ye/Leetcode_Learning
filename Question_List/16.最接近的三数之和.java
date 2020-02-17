4e7399023abafa0ccb48a83f42724ae1d4d40929
Hantao Ye <1507431786@qq.com>
2020-02-17 14:30:02 +0800

    update

--- Question_List/16.最接近的三数之和.java
+++ Question_List/16.最接近的三数之和.java
@@ -6,10 +6,16 @@
 
 // @lc code=start
 package Question_List;
+
+import java.util.*;
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int output = 0;
-        
+        int len = nums.length;
+        if (len == 0 || len < 3) {
+            
+        }
+        return output;
     }
 }
 // @lc code=end
