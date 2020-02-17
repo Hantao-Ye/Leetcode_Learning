a4c66d5ad6c755b53bf4fa15bf6839629a7e12a0
Hantao Ye <1507431786@qq.com>
2020-02-17 17:23:57 +0800

    the 16th problem

--- Question_List/16.最接近的三数之和.java
+++ Question_List/16.最接近的三数之和.java
@@ -8,15 +8,42 @@
 package Question_List;
 
 import java.util.*;
+
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int output = 0;
         int len = nums.length;
         if (len == 0 || len < 3) {
-            
+            for (int i = 0; i < len; i++)
+                output += nums[i];
+            return output;
+        }
+        Arrays.sort(nums);
+        output = Math.abs(target - nums[0]);
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > target)
+                break;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (Math.abs(sum - target) < output) {
+                    output = Math.abs(sum - target);
+                    while (L < R && nums[L] == nums[L + 1])
+                        L++; // 去重
+                    while (L < R && nums[R] == nums[R - 1])
+                        R--; // 去重
+                    L++;
+                    R--;
+                } else if (sum - target < 0)
+                    L++;
+                else if (sum - target > 0)
+                    R--;
+            }
         }
         return output;
     }
 }
 // @lc code=end
-
--- Question_List/7.整数反转.java
+++ Question_List/7.整数反转.java
@@ -8,14 +8,6 @@
 package Question_List;
 class Solution {
     public int reverse(int x) {
-        /*
-         * int output = 0; int sign = 1; StringBuffer sBuffer = new StringBuffer(); if
-         * (x < 0) { x = -x; sign = -sign; } while (x > 0) { sBuffer.append(x % 10); x
-         * /= 10; } String string = sBuffer.toString(); if (string.length() == 0) return
-         * 0; if (Long.parseLong(string)>Math.pow(2.0,31.0)&&sign == -1) return 0; else
-         * if (Long.parseLong(string) > (Math.pow(2.0, 31.0) + 1) && sign == 1) return
-         * 0; output = sign * Integer.valueOf(string); return output;
-         */
         long n = 0;
         while (x != 0) {
             n = n * 10 + x % 10;
