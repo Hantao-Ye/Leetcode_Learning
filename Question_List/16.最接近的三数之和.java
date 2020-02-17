932f8ca0a2500116a4c9a061bb5bf0e5f5088a28
Hantao Ye <1507431786@qq.com>
2020-02-17 17:44:49 +0800

    finish the 16th problem

--- Question_List/16.最接近的三数之和.java
+++ Question_List/16.最接近的三数之和.java
@@ -5,42 +5,32 @@
  */
 
 // @lc code=start
+// package Question_List;
 package Question_List;
-
 import java.util.*;
+
 
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int output = 0;
         int len = nums.length;
-        if (len == 0 || len < 3) {
-            for (int i = 0; i < len; i++)
-                output += nums[i];
-            return output;
-        }
         Arrays.sort(nums);
-        output = Math.abs(target - nums[0]);
-        for (int i = 0; i < len; i++) {
-            if (nums[i] > target)
-                break;
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+        int output = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < len-2; i++) {
             int L = i + 1;
             int R = len - 1;
             while (L < R) {
                 int sum = nums[i] + nums[L] + nums[R];
-                if (Math.abs(sum - target) < output) {
-                    output = Math.abs(sum - target);
-                    while (L < R && nums[L] == nums[L + 1])
-                        L++; // 去重
-                    while (L < R && nums[R] == nums[R - 1])
-                        R--; // 去重
+                if (sum - target < 0) {
+                    if (Math.abs(sum - target) < Math.abs(output - target))
+                        output = sum;
                     L++;
+                } else if (sum - target > 0) {
+                    if (Math.abs(sum - target) < Math.abs(output - target))
+                        output = sum;
                     R--;
-                } else if (sum - target < 0)
-                    L++;
-                else if (sum - target > 0)
-                    R--;
+                } else {
+                    return target;
+                }
             }
         }
         return output;
