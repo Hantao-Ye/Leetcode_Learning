d1d965bf64ded0fd54eebe7b15c0ca5fb6aca916
Hantao Ye <1507431786@qq.com>
2020-02-19 23:56:52 +0800

    update the 26th question

---
+++ Question_List/26.删除排序数组中的重复项.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=java
+ *
+ * [26] 删除排序数组中的重复项
+ */
+
+// @lc code=start
+package Question_List;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
+                nums[++i] = nums[j];
+            }
+            j++;
+        }
+        return ++i;
+    }
+}
+// @lc code=end
